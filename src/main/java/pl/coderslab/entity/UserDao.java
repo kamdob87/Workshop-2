1ced441eb84eeee29696d6de94045f810dadc8ee
kamil <kamdob87@gmail.com>
2023-10-11 12:00:42 +0200

    add class UserDao with querys

---
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -0,0 +1,11 @@
+package pl.coderslab.entity;
+
+public class UserDao {
+
+    private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY = "UPDATE users SET username = ?, email = ?, password = ?) WHERE id = ?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
+    private static final String FIND_ALL_USER_QUERY = "SELECT * FROM users";
+
+}
